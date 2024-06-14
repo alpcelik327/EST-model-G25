368a38d9d07af9b3c21c757cc52dad8cd4d0382b
alpcelik327 <a.celik1@student.tue.nl>
2024-06-14 15:20:38 +0200

    Update postprocessing.m

--- postprocessing.m
+++ postprocessing.m
@@ -1,79 +1,3 @@
-% % Post-processing script for the EST Simulink model. This script is invoked
-% % after the Simulink model is finished running (stopFcn callback function).
-% 
-% close all;
-% figure;
-% 
-% %% Supply and demand
-% subplot(2,2,1);
-% plot(tout/unit("day"), PSupply/unit("W"));
-% hold on;
-% plot(tout/unit("day"), PDemand/unit("W"));
-% xlim([0 tout(end)/unit("day")]);
-% grid on;
-% title('Supply and demand');
-% xlabel('Time [day]');
-% ylabel('Power [W]');
-% legend("Supply","Demand");
-% 
-% %% Stored energy
-% subplot(2,2,2);
-% plot(tout/unit("day"), EStorage/unit("J"));
-% xlim([0 tout(end)/unit("day")]);
-% grid on;
-% title('Storage');
-% xlabel('Time [day]');
-% ylabel('Energy [J]');
-% 
-% %% Energy losses
-% subplot(2,2,3);
-% plot(tout/unit("day"), D/unit("W"));
-% xlim([0 tout(end)/unit("day")]);
-% grid on;
-% title('Losses');
-% xlabel('Time [day]');
-% ylabel('Dissipation rate [W]');
-% 
-% %% Load balancing
-% subplot(2,2,4);
-% plot(tout/unit("day"), PSell/unit("W"));
-% hold on;
-% plot(tout/unit("day"), PBuy/unit("W"));
-% xlim([0 tout(end)/unit("day")]);
-% grid on;
-% title('Load balancing');
-% xlabel('Time [day]');
-% ylabel('Power [W]');
-% legend("Sell","Buy");
-% 
-% %% Pie charts
-% 
-% % integrate the power signals in time
-% EfromSupplyTransport = trapz(tout, PfromSupplyTransport);
-% EtoDemandTransport   = trapz(tout, PtoDemandTransport);
-% ESell                = trapz(tout, PSell);
-% EBuy                 = trapz(tout, PBuy);
-% EtoInjection         = trapz(tout, PtoInjection);
-% EfromExtraction      = trapz(tout, PfromExtraction);
-% EStorageDissipation  = trapz(tout, DStorage);
-% EDirect              = EfromSupplyTransport - ESell - EtoInjection;
-% ESurplus             = EtoInjection-EfromExtraction-EStorageDissipation;
-% 
-% figure;
-% tiles = tiledlayout(1,2);
-% 
-% ax = nexttile;
-% pie(ax, [EDirect, EtoInjection, ESell]/EfromSupplyTransport);
-% lgd = legend({"Direct to demand", "To storage", "Sold"});
-% lgd.Layout.Tile = "south";
-% title(sprintf("Received energy %3.2e [J]", EfromSupplyTransport/unit('J')));
-% 
-% ax = nexttile;
-% pie(ax, [EDirect, EfromExtraction, EBuy]/EtoDemandTransport);
-% lgd = legend({"Direct from supply", "From storage", "Bought"});
-% lgd.Layout.Tile = "south";
-% title(sprintf("Delivered energy %3.2e [J]", EtoDemandTransport/unit('J')));
-
 % Post-processing script for the EST Simulink model. This script is invoked
 % after the Simulink model is finished running (stopFcn callback function).
 
@@ -138,13 +62,14 @@ legend("Supply","Demand","Sell","Buy");
 
 %% Efficiency vs. time
 Efficiency = (PDemand - D) ./ PSupply * 100; % Calculate efficiency as percentage
+Efficiency = Efficiency / 1000; % Factor efficiency by a thousand
 subplot(3,3,6);
 plot(tout/unit("day"), Efficiency);
 xlim([0 tout(end)/unit("day")]);
 grid on;
 title('Efficiency vs. Time');
 xlabel('Time [day]');
-ylabel('Efficiency [%]');
+ylabel('Efficiency [per mil]'); % Changed to reflect the new unit
 
 %% Pie charts
 
@@ -167,7 +92,6 @@ pie(ax, [EDirect, EtoInjection, ESell]/EfromSupplyTransport);
 lgd = legend({"Direct to demand", "To storage", "Sold"});
 lgd.Layout.Tile = "south";
 title(sprintf("Received energy %3.2e [J]", EfromSupplyTransport/unit('J')));
-
 
 ax = nexttile;
 pie(ax, [EDirect, EfromExtraction, EBuy]/EtoDemandTransport);
